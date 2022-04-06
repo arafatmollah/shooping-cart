242e7fcf58699dd0086be8dbb971dcac341de089
Arafat MShuvo <arafatshuvo18@gmail.com>
2022-04-06 17:19:25 +0600

    issue solved

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('I am in a separate file')
+document.getElementById('phone-increase').addEventListener('click', function () {
+    const phonePrice = document.getElementById('phone-price');
+    const phonePriceText = phonePrice.innerText;
+    const phonePriceTextValue = parseInt(phonePriceText);
+    
+    const phonePriceValue = document.getElementById('price-value');
+    let priceValue = phonePriceValue.value;
+    priceValue = parseInt(priceValue) + 1; 
+    phonePriceValue.value = priceValue;
+
+    //balance
+    phonePrice.innerText = phonePriceTextValue * parseInt(priceValue);
+    // phonePrice.innerText = priceValue
+})
+document.getElementById('phone-decrease').addEventListener('click', function () {
+    const priceDecreased = document.getElementById('price-value');
+    const priceDecreasedValue = priceDecreased.value;
+    priceDecreased.value = parseInt(priceDecreasedValue) - 1;
+    
+    // const pricePhone = document.getElementById('phone-price');
+    // const priceText = pricePhone.innerText;
+    // const priceNumber = parseInt(priceText);
+    // pricePhone.innerText = priceNumber * parseInt(priceDecreasedValue)
+})
